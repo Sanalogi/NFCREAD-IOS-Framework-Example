0379d2a12b4fc27a36ec8e2866d7f198ac11ef9f
Sanalogi <sanalogi@sanalogi.com>
2021-08-24 17:25:33 +0300

    changes

--- ReaderExample/MrzController.swift
+++ ReaderExample/MrzController.swift
@@ -20,6 +20,7 @@ class MrzController: UIViewController,MrzScannerViewDelegate {
         
         scannerView.delegate = self
         NFCReader.sharedInstance.description = "NFCREAD-CameraScan"
+        NFCReader.sharedInstance.lang = "tr"
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -38,17 +39,50 @@ class MrzController: UIViewController,MrzScannerViewDelegate {
         self.dismiss(animated: true, completion: nil)
     }
     func mrzScannerView(_ mrzScannerView: MrzScannerView, didFind scanResult: MrzScanResult) {
-        if #available(iOS 13, *){
-            self.performSegue(withIdentifier: "segueToNfcScan", sender: nil)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM-dd-yyyy"
+        let birthDate = dateFormatter.string(from: scanResult.birthDate ?? Date())
+        let expDate = dateFormatter.string(from: scanResult.expiryDate ?? Date())
+        
+        
+        var str = ""
+        str += "Ad: " + scanResult.givenNames + "\n"
+        str += "Soyad: " + scanResult.surnames + "\n"
+        str += "mrz: " + scanResult.mrzString + "\n"
+        str += "docNumber: " + scanResult.documentNumber + "\n"
+        str += "birthDate: " + birthDate + "\n"
+        
+        str += "countryCode: " + scanResult.countryCode + "\n"
+        str += "docType: " + scanResult.documentType + "\n"
+        str += "Nationality: " + scanResult.nationality + "\n"
+        str += "expDate: " + expDate + "\n"
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            let alert = UIAlertController(title: "Title", message: str, preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK",
+                            style: .default,
+                            handler: { _ in
+                                if #available(iOS 13, *){
+                                    self.scanResult = scanResult
+                                    self.performSegue(withIdentifier: "segueToNfcScan", sender: nil)
+                                   
+                                }
+                                else{
+                                    self.scanResult = scanResult
+                                    self.performSegue(withIdentifier: "segueToMrzScan", sender: nil)
+                                    
+                                    print(scanResult.countryCode)
+                                    print(scanResult.documentNumber)
+                                    print(scanResult.givenNames)
+                                }
+                                
+            }))
+            self.present(alert, animated: true, completion: nil)
         }
-        else{
-            self.scanResult = scanResult
-            self.performSegue(withIdentifier: "segueToMrzScan", sender: nil)
-            
-            print(scanResult.countryCode)
-            print(scanResult.documentNumber)
-            print(scanResult.givenNames)
-        }
+        
+
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
--- ReaderExample/ViewController.swift
+++ ReaderExample/ViewController.swift
@@ -19,13 +19,36 @@ class ViewController: UIViewController,NFCReaderDelegate {
     @IBOutlet weak var imgView: UIImageView!
     
     func didSuccess(data: DocumentModel) {
+        var str = ""
+        str += "Soyad: " + data.lastName + "\n"
+        str += "Ad: " + data.firstName + "\n"
+        str += "docType: " + data.documentType + "\n"
+        str += "personalNo: " + data.personalNumber + "\n"
+        str += "DocNo: " + data.documentNumber + "\n"
+        str += "issuingAuthority: " + data.issuingAuthority + "\n"
+        str += "docExpDate: " + data.documentExpiryDate + "\n"
+        str += "DateOfBirth: " + data.dateOfBirth + "\n"
+        str += "Gender: " + data.gender + "\n"
+        str += "Nationality: " + data.nationality + "\n"
+        str += "issueDate: " + (data.issueDate ?? "") + "\n"
+        str += "placeBirth: " + (data.placeOfBirth ?? "") + "\n"
+        str += "Address: " + (data.address ?? "") + "\n"
+        str += "Telephone: " + (data.telephone ?? "") + "\n"
+        str += "Profession: " + (data.profession ?? "") + "\n"
+        str += "NFC READ COMPLETED!"
+
         
-        print(data.lastName)
-        print(data.firstName)
-        nameSurname.text = data.firstName + data.lastName
-        docNo.text = data.documentNumber
-        countryCode.text = data.nationality
-        imgView.image = data.passportImage
+//        nameSurname.text = data.firstName + data.lastName
+//        docNo.text = data.documentNumber
+//        countryCode.text = data.nationality
+//        imgView.image = data.passportImage
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            let alert = UIAlertController(title: "Title", message: str, preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+
     }
     
     func didFail(message: String) {
